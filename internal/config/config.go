f559c39b2eb0ff3e823a23de58bac92e44f108dd
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-18 19:00:04 +0300

    feat: add configuration loading with environment variables support

---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DatabaseURL string
+	Port string
+}
+
+func Load() (*Config, error) {
+	var err error = godotenv.Load()
+
+	if err != nil {
+		log.Println("[WARNING]: Using environment variable but .env file not found!")
+	}
+
+	var config *Config = &Config {
+		DatabaseURL: os.Getenv("DATABASE_URL"),
+		Port: os.Getenv("APP_PORT"),
+	}
+
+	return config, nil
+}
