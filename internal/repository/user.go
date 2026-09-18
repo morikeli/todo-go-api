60230435b9acaeec1262bf196276bcb9d2d7fe70
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-18 19:01:42 +0300

    feat: implement repository to create a new user

---
+++ internal/repository/user.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"context"
+	"time"
+	"todo-api/internal/models"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateUser(pool *pgxpool.Pool, user *models.User) (*models.User, error) {
+	var ctx context.Context
+	var cancel context.CancelFunc
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5 * time.Second)
+
+	defer cancel()
+
+	query := `
+		INSERT INTO users (email, password)
+		VALUES ($1, $2)
+		RETURNING id, email, created_at, updated_at
+	`
+
+	err := pool.QueryRow(ctx, query, user.Email, user.Password).Scan(
+		&user.ID,
+		&user.Email,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
